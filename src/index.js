d78cae124e4d394e78e8355d5d6c89f80f164088
idreamzzer <idreamzzer@gmail.com>
2018-04-26 15:46:48 +0300

    Fixed eslint problems

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './redux/configureStore';
 import App from './containers/App';
 import './global.css';
-
-// dotenv.config();
 
 const store = configureStore();
 
